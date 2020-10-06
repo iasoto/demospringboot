7be583cf46693fdfa6933ca3f19f79a0fa6d17e4
“iasoto” <iasoto@Ismaels-MacBook-Pro.local>
2020-10-06 15:53:10 -0500

    Primer commit

---
+++ src/main/java/com/uamc/demotalleruamc/DemotalleruamcApplication.java
@@ -0,0 +1,13 @@
+package com.uamc.demotalleruamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotalleruamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotalleruamcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uamc/demotalleruamc/DemotalleruamcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uamc.demotalleruamc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotalleruamcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
